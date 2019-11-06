c730487eedfa8ecc99e07e915359aecd3d8a2809
Bpmauge <briahanamauge@pursuit.org>
2019-11-06 16:08:37 -0500

    users routes complete

--- server/routes/users.js
+++ server/routes/users.js
@@ -10,7 +10,7 @@ const log = console.log;
 
 /* MODULE INITS */
 const express = require('express');
-    const router = express.Router();
+const router = express.Router();
 // Database 
 const db = require('../db.js');
 
@@ -20,22 +20,76 @@ const middleWare = async (req, res, next) => {
   try {
     let response = await db.any("SELECT * FROM users;");
     res.json({
-        status: "success",
-        message: req.get('host') + req.originalUrl,
-        body: response
+      status: "success",
+      message: req.get('host') + req.originalUrl,
+      body: response
     });
   } catch (error) {
     log(error);
     res.status(500).json({
-        status: "fail",
-        message: "Error: something went wrong"
+      status: "fail",
+      message: "Error: something went wrong"
+    });
+  }
+}
+
+const singleUser = async (req, res) => {
+  try {
+    let id = parseInt(req.params.id);
+    console.log(id);
+    let user = await db.any(`SELECT * FROM users WHERE user_id = '${id}'`);
+    console.log(user)
+    res.json({
+      payload: user,
+      message: `Here is the user!`
+    });
+  } catch (error) {
+    res.json({
+      message: `There was an error!`
+    });
+  }
+}
+
+const addUser = async (req, res) => {
+  try {
+    let insertQuery = `
+  INSERT INTO users(firstname, lastname, age)
+  VALUES($1, $2, $3)
+  `
+    await db.none(insertQuery, [req.body.firstname, req.body.lastname, req.body.age]);
+
+    res.json({
+      payload: req.body,
+      message: `User registration was successful!`
+    });
+  } catch (error) {
+    res.json({
+      message: `There was an error!`
+    });
+  }
+}
+
+const removeUser = async (req, res) => {
+  try {
+    let id = parseInt(req.params.id);
+    await db.none(`DELETE FROM users WHERE user_id = '${id}'`);
+    res.json({
+      message: `User was successfully deleted!`
+    });
+  } catch (error) {
+    res.json({
+      message: `There was an error!`
     });
   }
 }
 
 
 /* ROUTES */
-router.get("/", middleWare);
+router.get("/", middleWare); // get all users
+router.get("/:id", singleUser); //get single user
+router.post("/:id", addUser); // add a user
+router.delete("/:id", removeUser); // delete a user
+
 
 
 module.exports = router;
