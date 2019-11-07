f4b8876678edf9a1e6baaefbe2dcceb4d73389d4
KathyPuma <kathypuma96@gmail.com>
2019-11-06 21:44:19 -0500

    comments_complete

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,3 +1,5 @@
+
+
 /*
 Server Comments Route | HOLDING Web App
 GROUP 3: Douglas MacKrell, Briahana Mauge, Joseph P. Pasaoa, Kathy Puma
@@ -18,10 +20,10 @@ const db = require('../db.js');
 /* MIDDLEWARE */
 const middleWare = async (req, res, next) => {
   try {
-    let response = await db.any("SELECT * FROM users;");
+    let response = await db.any("SELECT * FROM comments;");
     res.json({
         status: "success",
-        message: req.get('host') + req.originalUrl,
+        message: "got all the comments",
         body: response
     });
   } catch (error) {
@@ -34,8 +36,106 @@ const middleWare = async (req, res, next) => {
 }
 
 
+
+const getAllCommentsFromASinglePost =  async(req, res) => {
+  let post_id = req.params.post_id
+  try {
+    let response = await db.any(`SELECT * FROM comments WHERE post_id = ${post_id}`);
+    // let response = await db.any(`SELECT body FROM comments WHERE post_id = ${post_id}`);
+    res.json({
+        status: "success",
+        message: `got all the comments from ${post_id}`,
+        body: response
+    });
+  } catch (error) {
+      res.status(500)
+      res.json({
+          message: "Error. Something went wrong"
+      })
+  }
+};
+
+
+
+
+const addASingleComment =  async(req, res) => {
+  try {
+    
+    let insertQuery = `
+    INSERT INTO comments(commenter_id, post_id,body)
+        VALUES($1, $2, $3)`
+
+      await db.none(insertQuery,[req.params.commenter_id, req.body.post_id, req.body.body]) 
+
+    res.json({
+        status: "success",
+        message: `Altered Sucess`,
+        body: req.body
+    });
+  } catch (error) {
+      res.status(500)
+      res.json({
+          message: "Error. Something went wrong"
+      })
+  }
+};
+
+
+
+
+const editASinglePost = async(req, res) => {
+  try {
+    let post_id = parseInt(req.params.post_id)
+    let comment_id = parseInt(req.params.comment_id)
+
+    let insertQuery = `
+    UPDATE comments 
+    SET body = ($1)
+    WHERE post_id=${post_id} AND comment_id = ${comment_id}`
+      await db.any(insertQuery,[req.body.body]) 
+
+    res.json({
+        status: "success",
+        message: `Altered Sucess`,
+        body: req.body
+    });
+  } catch (error) {
+      res.status(500)
+      res.json({
+          message: "Error. Something went wrong"
+          
+      })
+      console.log(error)
+  }
+};
+
+
+
+const deleteSingleComment=  async(req, res) => {
+  try {
+    let post_id = parseInt(req.params.post_id)
+    let comment_id = parseInt(req.params.comment_id)
+      await db.none(`DELETE FROM comments WHERE post_id= ${post_id} AND comment_id = ${comment_id}`) 
+    res.json({
+        status: "success",
+        message: `Delete Sucess`,
+        body: req.body
+    });
+  } catch (error) {
+      res.status(500)
+      res.json({
+          message: "Error. Something went wrong"
+      })
+      console.log(error)
+  }
+};
+
 /* ROUTES */
 router.get("/", middleWare);
+router.get("/posts/:post_id", getAllCommentsFromASinglePost);
+router.post('/posts/:post_id/:commenter_id',addASingleComment);
+router.patch('/:post_id/:comment_id', editASinglePost);
+router.delete('/:post_id/:comment_id', deleteSingleComment);
 
 
 module.exports = router;
