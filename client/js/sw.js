49d5865682216ca7fddce652670d0abf0f3155ba
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-11-12 16:11:45 -0500

    integrated hold xfer into userSim fx

--- client/js/sw.js
+++ client/js/sw.js
@@ -16,15 +16,38 @@ const swServerComm = async (method, urlAdds, body) => {
 
 
 /* USERHOLDS DROP DOWN FUNCS */
+const whatHoldAmIIn = () => {
+  const swUrlParams = (new URL(document.location)).searchParams;
+  return Number(swUrlParams.get('hold'));
+}
+
 const commForThisUserHolds = async (userId) => {
   return await swServerComm('get', `sw/userholds/${userId}`);
 } 
 
 const initHoldsDropper = async () => {
   const userId = document.querySelector('#userNum').value;
-  const dropperSelect = document.querySelector('holdsDropper');
-  let response = await commForThisUserHolds(userId);
-  console.log(response);
+  const dropperSelect = document.querySelector('#holdsDropper');
+  const response = await commForThisUserHolds(userId);
+  const dataArr = response.body;
+  if (dataArr.length < 1) {
+    dropperSelect.style.display = "none";
+  } else {
+    for (let holdObj of dataArr) {
+      let makingOpt = document.createElement('option');
+      makingOpt.innerText = holdObj.name;
+      makingOpt.value = holdObj.hold_id;
+      if (whatHoldAmIIn() === holdObj.hold_id) {
+        makingOpt.selected = "selected";
+      }
+      dropperSelect.appendChild(makingOpt);
+    }
+    dropperSelect.addEventListener("change", (e) => {
+        const holdGoingTo = e.target.options[e.target.selectedIndex].value;
+        // var text = e.target.options[e.target.selectedIndex].text; // saved for future display of Hold Name
+
+    });
+  }
 } 
 
 
